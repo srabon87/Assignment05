d0d91418aeebde67f9aa1635a5290429ee33e603
srabon87 <srabondas199816@gmail.com>
2024-09-25 22:03:39 +0600

    added more changes in HTML and JS files

--- js/index.js
+++ js/index.js
@@ -1,17 +1,80 @@
-const btn1 = document.getElementById('calculateone').addEventListener('click',function(){
+// const btn1 = document.getElementById('calculateone').addEventListener('click',function(){
 
-    const addMoneyInput = parseFloat(document.getElementById('input-add-money').value);
+//     const addMoneyInput = parseFloat(document.getElementById('input-add-money').value);
 
-    const addMoneyOutput = parseFloat(document.getElementById('donate-balance').innerText);
-    const addMoney = parseFloat(document.getElementById('main_balance').innerText);
+//     const addMoneyOutput = parseFloat(document.getElementById('donate-balance').innerText);
+//     const addMoney = parseFloat(document.getElementById('main_balance').innerText);
 
-    const newBalance = addMoneyInput + addMoneyOutput;
+//     const newBalance = addMoneyInput + addMoneyOutput;
     
-    document.getElementById('donate-balance').innerText = newBalance;
-    const updateBalance = addMoney - addMoneyInput;
-    document.getElementById('main_balance').innerText = updateBalance;
+//     document.getElementById('donate-balance').innerText = newBalance;
+//     const updateBalance = addMoney - addMoneyInput;
+//     document.getElementById('main_balance').innerText = updateBalance;
 
-    console.log(updateBalance);
+//     console.log(updateBalance);
+
+//     if(addMoneyInput <= 0){
+//         alert('Invalid input')
+//     }
     
+// })
+// const btn2 = document.getElementById('calculateone1').addEventListener('click',function(){
+
+//     const addMoneyInput1 = parseFloat(document.getElementById('input-add-money1').value);
+
+//     const addMoneyOutput1 = parseFloat(document.getElementById('donate-balance1').innerText);
+//     const addMoney = parseFloat(document.getElementById('main_balance').innerText);
+
+//     const newBalance1 = addMoneyInput1 + addMoneyOutput1;
     
+//     document.getElementById('donate-balance1').innerText = newBalance1;
+//     const updateBalance1 = addMoney - addMoneyInput1;
+//     document.getElementById('main_balance').innerText = updateBalance1;
+
+//     console.log(updateBalance1);
+    
+//     if(addMoneyInput1 <= 0){
+//         alert('Invalid input')
+//     }
+// })
+// const btn3 = document.getElementById('calculateone2').addEventListener('click',function(){
+
+//     const addMoneyInput2 = parseFloat(document.getElementById('input-add-money2').value);
+
+//     const addMoneyOutput2 = parseFloat(document.getElementById('donate-balance2').innerText);
+//     const addMoney = parseFloat(document.getElementById('main_balance').innerText);
+
+//     const newBalance2 = addMoneyInput2 + addMoneyOutput2;
+    
+//     document.getElementById('donate-balance2').innerText = newBalance2;
+//     const updateBalance2 = addMoney - addMoneyInput2;
+//     document.getElementById('main_balance').innerText = updateBalance2;
+
+//     console.log(updateBalance2);
+    
+//     if(addMoneyInput2 <= 0){
+//         alert('Invalid input')
+//     }
+// })
+
+
+getID("donation-btn").addEventListener("click",()=>{
+    getID("donate-section").classList.remove("hidden");
+    getID("history-section").classList.add("hidden");
+})
+getID("history-btn").addEventListener("click",()=>{
+    getID("donate-section").classList.add("hidden");
+    getID("history-section").classList.remove("hidden");
+})
+
+
+// calculte donate Amount
+getID("calculate-one").addEventListener("click",()=>{
+    calculateFunc("main-balance","donate-balance-one","add-money-one","title-one")
+})
+getID("calculate-two").addEventListener("click",()=>{
+    calculateFunc("main-balance","donate-balance-two","add-money-two","title-two")
+})
+getID("calculate-three").addEventListener("click",()=>{
+    calculateFunc("main-balance","donate-balance-three","add-money-three","title-three")
 })
--- js/utilize .js
+++ js/utilize .js
@@ -1,48 +1,61 @@
-const historyBtn = document.getElementById('history-btn').addEventListener('click', function(){
-    
-    historyBtn.classList.add(
-        'bg-lime-400')
-    
-    console.log('section done');
-    
-})
+function getID(id){
+    return document.getElementById(id);
+}
+function getValue(id){
+    let inputValue = getID(id).value;
+    return parseFloat(inputValue);
+}
 
-// const donationBtn = document.getElementById("donation-btn");
-// const historyBtn = document.getElementById("history-btn");
-//     donationBtn.addEventListener('click',function(){
+function calculateFunc(abalance,donatedBalance,donateNowBalance,title){
+    let inputAmout = getID(donateNowBalance).value;
+    // condition check
+    if(!isNaN(inputAmout) ){
+        let allDontedBalance = parseFloat(getID(donatedBalance).innerText); 
+        let accoundBalance = parseFloat(getID(abalance).innerText);
+        let userDonateAmount = getValue(donateNowBalance);
+        if(accoundBalance > userDonateAmount && userDonateAmount > 0){
+            let newABalance = (accoundBalance - userDonateAmount);
+            let newDonatedAdd = (allDontedBalance + userDonateAmount);
 
-//     donationBtn.classList.add(
-//             "text-black",
-//             "bg-lime-400",
+        // new account balance add
+        getID(abalance).innerText = newABalance;
 
-//         )
-//     historyBtn.classList.remove("text-gray-600")
-       
-//     historyBtn.classList.remove(
-//         "text-black",
-//         "bg-lime-400",
-//     )
-//     historyBtn.classList.add("text-gray-600");
+        // donated balance add you userdonateAmount
+        getID(donatedBalance).innerText = newDonatedAdd;
 
-//     document.getElementById("sections").classList.remove("hidden",'sections');
-//     document.getElementById("sections-new").classList.add("hidden");
-// })         
+        // date 
+        let makeDate = new Date()
+        // replace title 
+        let titleText = getID(title).innerText;
+        let repTitle = titleText.replace("Donate for",'')
+        // create history transition
+        let div = document.createElement("div");
+        div.classList.add(
+            "space-y-3",
+            "border-2",
+            "p-4",
+            "rounded-lg",
+            "border-zinc-500",
+            "text-black"
+        )
+        div.innerHTML = `
+            <h3 class="font-semibold text-2xl ">${userDonateAmount} tk donated for ${repTitle}</h3>
+            <p class="text-lg font-light p-3 bg-blue-200 rounded-lg">Date: ${makeDate}</p>
+        `;
+        let showHistory = getID("all-history")
+        showHistory.insertBefore(div,showHistory.childNodes[0])
 
+        getID(donateNowBalance).value = '';
 
-//     historyBtn.addEventListener('click',function(){
+        // modal show
+        getID("my_modal_6").checked = true;
+        } else{
+            return getID("my_modal_3").checked = true;
+        }
+        
+        }
 
-//         historyBtn.classList.add(
-//             "text-black",
-//             "bg-lime-400",
-
-//         )
-       
-//         donationBtn.classList.remove(
-//         "text-black",
-//         "bg-lime-400",
-//     )
-    
-
-//     document.getElementById("sections").classList.add("hidden");
-//     document.getElementById("sections-new").classList.remove("hidden");
-// })         
+        else{
+            return getID("my_modal_5").checked = true;
+        }
+    }
